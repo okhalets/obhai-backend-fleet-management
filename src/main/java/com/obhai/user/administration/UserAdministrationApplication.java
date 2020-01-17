290a1b2a1cbe61859450c4f880315c5757bc50a7
okhalets <okhalets@softserveinc.com>
2020-01-17 14:52:53 +0200

    Initial commit

---
+++ src/main/java/com/obhai/user/administration/UserAdministrationApplication.java
@@ -0,0 +1,13 @@
+package com.obhai.user.administration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAdministrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserAdministrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/obhai/user/administration/UserAdministrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obhai.user.administration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAdministrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
